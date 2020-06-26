bbc91fb5c9d4085f3dbe4410c02bf13897e08472
channaveerayya <channucs0009@gmail.com>
2020-06-26 15:01:42 +0530

    completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_gui/result.dart';
+
+import './quiz.dart';
+import './result.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,40 +16,67 @@ class MyApp extends StatefulWidget {
 }
 
 class MyAppState extends State<MyApp> {
-   var nextQuestion = 0;
-    void answers() {
-      setState(() {
-        nextQuestion++;
-      });
-      print(nextQuestion);
+  var nextQuestion = 0;
+  var _totalScore = 0;
+  final _questions = [
+    {
+      'questionText': 'What\'s your favorite Color',
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 1}
+      ],
+    },
+    {
+      'questionText': 'What\'s your favorite Animal',
+      'answers': [
+        {'text': 'Lion', 'score': 40},
+        {'text': 'Tiger', 'score': 30},
+        {'text': 'Elephant', 'score': 20},
+        {'text': 'Snake', 'score': 10}
+      ],
+    },
+    {
+      'questionText': 'What\'s your favorite Car',
+      'answers': [
+        {'text': 'Mits', 'score': 30},
+        {'text': 'Fer', 'score': 20},
+        {'text': 'Lamb', 'score': 10}
+      ],
     }
+  ];
+
+  void _answerQuestion(int score) {
+    _totalScore += score;
+    setState(() {
+      nextQuestion++;
+    });
+    print(nextQuestion);
+  }
+
+  void _resetQuize() {
+    setState(() {
+      nextQuestion = 0;
+      _totalScore = 0;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    var questions = ['what your favorite color', 'what your favorite animal'];
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('My First App'),
-          ),
-          body: Column(
-            children: [
-              Text(questions[nextQuestion]),
-              RaisedButton(
-                child: Text("Ans 1"),
-                onPressed: () => print("ans 1"),
-              ),
-              RaisedButton(
-                child: Text("Ans 2"),
-                onPressed: () {
-                  print("ans 2");
-                },
-              ),
-              RaisedButton(
-                child: Text("Ans 3"),
-                onPressed: answers,
-              ),
-            ],
-          )),
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: nextQuestion < _questions.length
+            ? Quiz(
+                anserQuestion: _answerQuestion,
+                questionIndex: nextQuestion,
+                questions: _questions,
+              )
+            : Result(_totalScore,_resetQuize),
+      ),
     );
   }
 }
